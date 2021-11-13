1352885cb4dfc45b90052d43909766355c24be88
Rakesh B N <68104336+Rakesh-1-7@users.noreply.github.com>
2021-11-13 19:33:30 +0530

    Add files via upload

---
+++ 1_st_program.js
@@ -0,0 +1,11 @@
+// the hello world program
+
+function fToC()
+{
+  var deg_c, deg_c;
+
+  deg_f = parseInt(document.getElementById("farenheit").value);
+  deg_c = 5*(deg_f-32)/9.0;
+  document.getElementById("answer").value = deg_c;
+}
+
