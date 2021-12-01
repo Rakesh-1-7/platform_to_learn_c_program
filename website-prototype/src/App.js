2bac602877284c43364e5c8183dde97b1d267f14
Rakesh B N <68104336+Rakesh-1-7@users.noreply.github.com>
2021-12-01 21:43:35 +0530

    Add files via upload

---
+++ website-prototype/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Header from './MyComponents/Header';
+
+function App() {
+
+  return (
+    <>
+    <Header title="C Programming" searchBar={false}/>
+    {/* <Footer/> */}
+    {/* <TodoItem/> */}
+    </>
+  );
+}
+
+
+export default App;
