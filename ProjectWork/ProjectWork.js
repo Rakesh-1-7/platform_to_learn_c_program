809935a3b1d4fae13b8cde4da88997c1910c2b2c
Riteesh-A <84441203+Riteesh-A@users.noreply.github.com>
2021-12-07 22:23:57 +0530

    Add files via upload
    Project Work elements by riteesh

---
+++ ProjectWork/ProjectWork.js
@@ -0,0 +1,7 @@
+function fToC() {
+    var deg_c, deg_c;
+
+    deg_f = parseInt(document.getElementById("farenheit").value);
+    deg_c = 5 * (deg_f - 32) / 9.0;
+    document.getElementById("answer").value = deg_c;
+}
