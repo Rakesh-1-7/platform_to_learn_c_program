6b1e495c21b1a43779f43e239aa522f6c5cf488b
Rakesh B N <68104336+Rakesh-1-7@users.noreply.github.com>
2021-12-10 08:55:52 +0530

    Add files via upload

---
+++ ezC_v1/login/loginpage.js
@@ -0,0 +1,4 @@
+
+     function onSubmit() {
+        if (document.getElementById('password').value == '12345678') {window.location.href = 'https://www.google.co.in/'; }else{ alert('Please check your passcode and try again');}
+   }
