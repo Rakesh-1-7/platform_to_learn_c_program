323604db6d037b51af5bf6476a094164e8e5c619
Rakesh B N <68104336+Rakesh-1-7@users.noreply.github.com>
2021-12-24 10:47:07 +0530

    Add files via upload

--- ezC_v1/login/login.js
+++ ezC_v1/login/login.js
@@ -3,3 +3,25 @@ function toggleForm()
     var container = document.querySelector('.container');
     container.classList.toggle('active')
 }
+
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
+
+togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye / eye slash icon
+    this.classList.toggle('bi-eye');
+});
+
+const togglePasswordLogin = document.querySelector('#togglePasswordlogin');
+const passwordLogin = document.querySelector('#password');
+
+togglePasswordLogin.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = passwordLogin.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordLogin.setAttribute('type', type);
+    // toggle the eye / eye slash icon
+    this.classList.toggle('bi-eye');
+});
