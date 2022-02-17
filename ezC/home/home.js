5e3239e231f2082f0ada56402473fd54f1193ea6
Rakesh B N <68104336+Rakesh-1-7@users.noreply.github.com>
2022-02-17 14:51:41 +0530

    Add files via upload

--- ezC/home/home.js
+++ ezC/home/home.js
@@ -25,4 +25,26 @@ function LoginFormexit() {
 
 }
 
+document.getElementById("passwd-conditions").style.display = "none";
+function validatePassword() {
+    var user = document.getElementById('newuser').value;
+    var email = document.getElementById('newemail').value;
+    var regularExpression  = /^[a-zA-Z0-9!@#$%^&*]{8,20}$/;
+    var Password = document.getElementById('passwd').value;
+    var PasswordConfirm = document.getElementById('confirm_password').value;
+    console.log("out");
+    if(!regularExpression.test(Password) && user != "" && email != "") {
+        console.log("in");
+        document.getElementById("passwd-conditions").style.display = "";
 
+        setTimeout(function(){
+            document.getElementById("passwd-conditions").style.display = "none";
+
+        }, 3500); 
+
+    }
+    if(Password != PasswordConfirm)
+    {
+        
+    }
+}
